d4f85e04fa1e4e5919dc73e92d73c09116396b9d
Guillaume Simard <guisim@gmail.com>
2013-10-01 23:35:26 -0400

    + Added rodin.js + Added rodin.ts
    + Can retrieve a logger by name
    + Can log under trace, debug, info, warn and error
    + Can change a logger's level
    + Can change default logger levels

---
+++ js/rondin.js
@@ -0,0 +1,119 @@
+var LogLevel;
+(function (LogLevel) {
+    LogLevel[LogLevel["TRACE"] = 100] = "TRACE";
+    LogLevel[LogLevel["DEBUG"] = 200] = "DEBUG";
+    LogLevel[LogLevel["INFO"] = 300] = "INFO";
+    LogLevel[LogLevel["WARN"] = 400] = "WARN";
+    LogLevel[LogLevel["ERROR"] = 500] = "ERROR";
+    LogLevel[LogLevel["OFF"] = 1000000] = "OFF";
+})(LogLevel || (LogLevel = {}));
+
+var Level = (function () {
+    function Level(level, logFunction, style) {
+        this.level = level;
+        this.logFunction = logFunction;
+        this.style = style;
+    }
+    return Level;
+})();
+
+var Logger = (function () {
+    function Logger(name, level) {
+        this.name = name;
+        this.level = level;
+    }
+    Logger.prototype.log = function (message, level, objects) {
+        if (this.level <= level.level) {
+            console.log(this.level + " <= " + level.level);
+            var params = ["%c" + LogLevel[level.level] + " - " + message, level.style];
+            params = params.concat(objects);
+            level.logFunction(params);
+        }
+    };
+
+    Logger.prototype.trace = function (message) {
+        var objects = [];
+        for (var _i = 0; _i < (arguments.length - 1); _i++) {
+            objects[_i] = arguments[_i + 1];
+        }
+        this.log(message, Logger.TRACE, objects);
+    };
+
+    Logger.prototype.debug = function (message) {
+        var objects = [];
+        for (var _i = 0; _i < (arguments.length - 1); _i++) {
+            objects[_i] = arguments[_i + 1];
+        }
+        this.log(message, Logger.DEBUG, objects);
+    };
+
+    Logger.prototype.info = function (message) {
+        var objects = [];
+        for (var _i = 0; _i < (arguments.length - 1); _i++) {
+            objects[_i] = arguments[_i + 1];
+        }
+        this.log(message, Logger.INFO, objects);
+    };
+
+    Logger.prototype.warn = function (message) {
+        var objects = [];
+        for (var _i = 0; _i < (arguments.length - 1); _i++) {
+            objects[_i] = arguments[_i + 1];
+        }
+        this.log(message, Logger.WARN, objects);
+    };
+
+    Logger.prototype.error = function (message) {
+        var objects = [];
+        for (var _i = 0; _i < (arguments.length - 1); _i++) {
+            objects[_i] = arguments[_i + 1];
+        }
+        this.log(message, Logger.ERROR, objects);
+    };
+    Logger.normalLog = function (objects) {
+        return console.log.apply(console, objects);
+    };
+    Logger.warnLog = function (objects) {
+        return console.warn.apply(console, objects);
+    };
+    Logger.errorLog = function (objects) {
+        return console.error.apply(console, objects);
+    };
+
+    Logger.TRACE = new Level(LogLevel.TRACE, Logger.normalLog, "color:#AAA");
+    Logger.DEBUG = new Level(LogLevel.DEBUG, Logger.normalLog, "");
+    Logger.INFO = new Level(LogLevel.INFO, Logger.normalLog, "font-weight:bold");
+    Logger.WARN = new Level(LogLevel.WARN, Logger.warnLog, "");
+    Logger.ERROR = new Level(LogLevel.ERROR, Logger.errorLog, "");
+    return Logger;
+})();
+
+var Rondin = (function () {
+    function Rondin() {
+    }
+    Rondin.get = function (name) {
+        if (!Rondin.loggers[name]) {
+            Rondin.loggers[name] = new Logger(name, Rondin.defaultLogLevel);
+        }
+        return Rondin.loggers[name];
+    };
+
+    Rondin.setDefaultLogLevel = function (newDefaultLevel, applyToExistingLoggers) {
+        if (typeof applyToExistingLoggers === "undefined") { applyToExistingLoggers = false; }
+        Rondin.defaultLogLevel = newDefaultLevel;
+        if (applyToExistingLoggers) {
+            console.log("!");
+            for (var loggerName in Rondin.loggers) {
+                var logger = Rondin.loggers[loggerName];
+                logger.level = Rondin.defaultLogLevel;
+            }
+        }
+    };
+    Rondin.loggers = {};
+    Rondin.defaultLogLevel = LogLevel.INFO;
+    return Rondin;
+})();
+
+var logger = Rondin.get("awesome.js");
+Rondin.setDefaultLogLevel(LogLevel.TRACE);
+logger.trace("Popo!");
---
+++ ts/rondin.ts
@@ -0,0 +1,92 @@
+
+enum LogLevel {
+	TRACE = 100,
+	DEBUG = 200,
+	INFO = 300,
+	WARN = 400,
+	ERROR = 500,
+	OFF = 1000000 // Arbitrarly large integer
+}
+
+class Level {
+	constructor(public level:LogLevel, 
+				public logFunction:(objects:any[]) => void, 
+				public style:string) {
+		
+	}
+}
+
+class Logger {
+
+	 static normalLog = (objects:any[]) =>	console.log.apply(console, objects);	
+	 static warnLog = (objects:any[]) =>	console.warn.apply(console, objects);	
+	 static errorLog = (objects:any[]) =>	console.error.apply(console, objects);
+	
+	 static TRACE = new Level(LogLevel.TRACE, Logger.normalLog,"color:#AAA");
+	 static DEBUG = new Level(LogLevel.DEBUG, Logger.normalLog,"");
+	 static INFO = new Level(LogLevel.INFO, Logger.normalLog,"font-weight:bold");
+	 static WARN = new Level(LogLevel.WARN, Logger.warnLog, "");
+	 static ERROR = new Level(LogLevel.ERROR, Logger.errorLog, "");
+	 
+	 constructor(public name: string, public level:LogLevel){
+		 
+	 }
+	 
+	 log(message:string, level:Level, objects:any[]) {
+		 if (this.level <= level.level) {
+			 console.log(this.level + " <= " + level.level)
+			 var params = ["%c" + LogLevel[level.level] + " - " + message, level.style];
+			 params = params.concat(objects);
+			 level.logFunction(params)
+		 }
+	 }
+	 
+	 trace(message:string, ...objects:any[]) {
+		 this.log(message, Logger.TRACE, objects);
+	 }
+	 
+	 debug(message:string, ...objects:any[]) {
+		 this.log(message, Logger.DEBUG, objects);
+	 }
+	 
+	 info(message:string, ...objects:any[]) {
+		 this.log(message, Logger.INFO, objects);
+	 }
+	 
+	 warn(message:string, ...objects:any[]) {
+		 this.log(message, Logger.WARN, objects);
+	 }
+	 
+	 error(message:string, ...objects:any[]) {
+		 this.log(message, Logger.ERROR, objects);
+	 }
+}
+
+class Rondin {
+	 private static loggers: { [name:string]: Logger} = {};
+	 private static defaultLogLevel: LogLevel = LogLevel.INFO; 
+	
+ 	 static get(name:string) {
+		if (!Rondin.loggers[name]) {
+			Rondin.loggers[name] = new Logger(name, Rondin.defaultLogLevel);
+		}
+		return Rondin.loggers[name];
+	}
+	
+	static setDefaultLogLevel(newDefaultLevel:LogLevel, applyToExistingLoggers:boolean = false) {
+		
+		Rondin.defaultLogLevel = newDefaultLevel;
+		if (applyToExistingLoggers) {
+			console.log("!");
+			for (var loggerName in Rondin.loggers) {
+				var logger:Logger = Rondin.loggers[loggerName];
+				logger.level = Rondin.defaultLogLevel;
+			}
+		}
+	}
+}
+
+
+var logger = Rondin.get("awesome.js");
+Rondin.setDefaultLogLevel(LogLevel.TRACE);
+logger.trace("Popo!");
